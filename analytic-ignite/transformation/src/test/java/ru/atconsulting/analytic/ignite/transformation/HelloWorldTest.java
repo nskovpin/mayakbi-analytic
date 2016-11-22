66146354a2b00b28ee56d2de8e945a0534f2e995
Сковпин Николай Сергеевич <nskovpin@at-consulting.ru>
2016-11-22 10:24:41 +0300

    #mayakbi-analytic - init commit

---
+++ analytic-ignite/transformation/src/test/java/ru/atconsulting/analytic/ignite/transformation/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package ru.atconsulting.analytic.ignite.transformation;
+
+import org.junit.Test;
+
+/**
+ * Created by NSkovpin on 22.11.2016.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+}
